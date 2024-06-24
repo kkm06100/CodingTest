3e282ed75785d890b62f8361ddbefd522dcee369
김경민 <kkm07133@naver.com>
2024-06-24 20:17:01 +0900

    [level 2] Title: 구명보트, Time: 32.49 ms, Memory: 55.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        List<Integer> peoples = Arrays.stream(people).sorted().boxed().collect(Collectors.toList());
+        while(peoples.size()>1){
+            if(peoples.get(0) + peoples.get(peoples.size()-1)<=limit){
+                peoples.remove(0);
+                peoples.remove(peoples.size()-1);
+                answer ++;
+            }
+            else {
+                peoples.remove(peoples.size()-1);
+                answer++;
+            }
+        }
+        if(peoples.size()==1){
+            answer++;
+        }
+        
+        
+        return answer;
+    }
+}
