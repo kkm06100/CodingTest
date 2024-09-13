1ffbcc845da6dafffefa64c8b0741c5f42de0e43
김경민 <kkm07133@naver.com>
2024-09-13 12:16:54 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,5 +1,16 @@
 function solution(my_string, is_suffix) {
-    var answer = 0;
-    answer = my_string.endsWith(is_suffix)?1:0;
+    var answer = 1;
+    // answer = my_string.endsWith(is_suffix)?1:0;
+    
+        var index_str = my_string.length-1;
+        if(my_string.length<is_suffix.length){
+            return 0;
+        }
+        for(var i = is_suffix.length-1;i>=0;i--){
+            if(my_string.charAt(index_str--) != is_suffix.charAt(i)){
+                return 0;
+            }
+        }
+        return 1;
     return answer;
 }
