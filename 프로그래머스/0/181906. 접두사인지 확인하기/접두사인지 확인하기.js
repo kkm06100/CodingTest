c205fe08283d7d1a9e553bd53533e5af56bea73e
김경민 <kkm07133@naver.com>
2024-09-13 11:56:24 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -1,9 +1,10 @@
 function solution(my_string, is_prefix) {
     var answer = 1;
-    for(var i = 0; i < is_prefix.length; i++){
-        if(is_prefix.charAt(i) != my_string.charAt(i)){
-            return 0;
-        }
-    }
+    answer = my_string.startsWith(is_prefix)?1:0;
+    // for(var i = 0; i < is_prefix.length; i++){
+    //     if(is_prefix.charAt(i) != my_string.charAt(i)){
+    //         return 0;
+    //     }
+    // }
     return answer;
 }
