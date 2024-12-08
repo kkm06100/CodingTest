1859731a6f82137b9a077d682ad007a6b7de910f
김경민 <kkm07133@naver.com>
2024-12-08 21:46:34 +0900

    [Bronze V] Title: 새싹, Time: 104 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        System.out.println("         ,r\'\"7");
+        System.out.println("r`-_   ,\'  ,/");
+        System.out.println(" \\. \". L_r\'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+
+    }
+}
