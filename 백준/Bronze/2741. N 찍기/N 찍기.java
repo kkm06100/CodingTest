c2d6c6e95587a9036382d1233f8c65bc05103165
김경민 <kkm07133@naver.com>
2024-12-10 23:17:08 +0900

    [Bronze V] Title: N 찍기, Time: 788 ms, Memory: 33048 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        for(int i = 1; i <= N;i ++){
+            System.out.println(i);
+        }
+    }
+}
