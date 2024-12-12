5caf87f34edf922c6b5443374896fbe82350abd0
김경민 <kkm07133@naver.com>
2024-12-12 21:52:49 +0900

    [Bronze V] Title: 엄청난 부자2, Time: 132 ms, Memory: 16452 KB -BaekjoonHub

---
+++ 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.BigInteger;
+
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BigInteger n = new BigInteger(st.nextToken());
+        BigInteger m = new BigInteger(st.nextToken());
+        System.out.print(n.divide(m) + "\n" + n.remainder(m));
+    }
+}
