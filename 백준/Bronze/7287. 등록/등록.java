2d41630fa1ddf12ec53266d36db3f68fb3d4c195
김경민 <kkm07133@naver.com>
2024-12-15 21:00:15 +0900

    [Bronze V] Title: 등록, Time: 96 ms, Memory: 13960 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        System.out.println("51");
+        System.out.println("kkm06100");
+    }
+}
