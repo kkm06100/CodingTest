9c319d01f4e9b332d749a818ef9f516c24367262
김경민 <kkm07133@naver.com>
2024-12-25 20:57:49 +0900

    [Bronze V] Title: 콜센터, Time: 104 ms, Memory: 14024 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws IOException{
+        System.out.println("     /~\\");
+        System.out.println("    ( oo|");
+        System.out.println("    _\\=/_");
+        System.out.println("   /  _  \\");
+        System.out.println("  //|/.\\|\\\\");
+        System.out.println(" ||  \\ /  ||");
+        System.out.println("============");
+        System.out.println("|          |");
+        System.out.println("|          |");
+        System.out.println("|          |");
+    }
+}
