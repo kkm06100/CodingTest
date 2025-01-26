2451f30df2128461fa70345d69405cdf85474b66
김경민 <kkm07133@naver.com>
2025-01-26 23:34:51 +0900

    [Bronze V] Title: 꼬마 정민, Time: 172 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        long f = scanner.nextLong();
+        long s = scanner.nextLong();
+        long t = scanner.nextLong();
+        System.out.println(f+s+t);
+    }
+}
