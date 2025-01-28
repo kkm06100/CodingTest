64e323cac242ca0a1b5a0a78ea6a77f9a8bcc529
김경민 <kkm07133@naver.com>
2025-01-28 23:46:44 +0900

    [Bronze V] Title: 오늘 날짜, Time: 164 ms, Memory: 17228 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,7 @@
+import java.time.LocalDate;import java.time.ZoneId; 
+public class Main {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+        System.out.println(now); 
+    }
+}
