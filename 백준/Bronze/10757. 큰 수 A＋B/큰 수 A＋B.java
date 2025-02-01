db2fc4d77288a406b025ec3e8ff0dbadb73cbb5d
김경민 <kkm07133@naver.com>
2025-02-01 17:35:37 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 360 ms, Memory: 20536 KB -BaekjoonHub

---
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+        System.out.println(a.add(b));
+
+    }
+}
