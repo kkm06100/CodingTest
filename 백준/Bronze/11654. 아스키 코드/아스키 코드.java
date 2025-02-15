186236c062d4f1bd356cf9f842dd842c5a862c13
김경민 <kkm07133@naver.com>
2025-02-15 20:47:54 +0900

    [Bronze V] Title: 아스키 코드, Time: 172 ms, Memory: 17604 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+		String s = scanner.next();
+		int c = s.charAt(0);
+        System.out.println(c);
+    }
+}
