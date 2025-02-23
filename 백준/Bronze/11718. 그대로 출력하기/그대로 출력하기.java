364e5002361fc195f882e76ace833720beafa755
김경민 <kkm07133@naver.com>
2025-02-23 19:10:03 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 184 ms, Memory: 17992 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int factorial = 1;
+        while(scanner.hasNextLine()){
+            System.out.println(scanner.nextLine());
+        }
+    }
+}
