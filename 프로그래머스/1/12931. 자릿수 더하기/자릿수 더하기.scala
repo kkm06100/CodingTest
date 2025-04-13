30fd0b6c3cd5305d5840d3bfc9050865a8e195df
devkyungmin <kkm07133@naver.com>
2025-04-13 14:15:41 +0900

    [level 1] Title: 자릿수 더하기, Time: 1.22 ms, Memory: 93 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.scala
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.scala
@@ -1,11 +1,6 @@
 object Solution {
     
     def solution(n: Int): Int = {
-        var answer = 0
-        if(n < 10) {
-            n
-        }else {
-            n%10 + solution(n/10)
-        }
+        if(n < 10) n else n % 10 + solution(n/10)
     }
 }
