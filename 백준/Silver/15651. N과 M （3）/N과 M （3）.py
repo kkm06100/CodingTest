57066038550f000d04db4e3251229bc2a9f29278
ḵ̷͉̥͓͉̫̥̲͓̇̒̆y̶̛͓̖͍͐͐̃̃͊͘͜͝ū̸͈̍̑͘n̴͕͓̫̟͋̀g̵͍̻̙̹̠͐̉͗͗́m̷͎͚͓͓͖̫̭̝͌͜į̶̛̲͔̲̬̩͆̌̑̒͑̕ň̶̤̱͙͇͠ <kkm07133@naver.com>
2025-08-25 20:28:55 +0900

    [Silver III] Title: N과 M (3), Time: 2592 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,17 @@
+numbers = input().split()
+N = int(numbers[0])
+M = int(numbers[1])
+
+def factorial(arr: list):
+    if len(arr) == M:
+        for value in arr:
+            print(value, end=' ')
+        print()
+        return
+
+    for i in range(1, N+1):
+        factorial(arr + [i])
+
+
+for i in range(1, N+1):
+    factorial(arr=[i])
